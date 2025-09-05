beb99fa9edd853011264e46e6386ab5f33ab4ff2
bahman <bja@adlas.at>
2025-09-05 13:48:53 +0330

    feat(bootstrap): append AuthMiddleware to application middleware
    - Add AuthMiddleware to the application's middleware stack
    - This change enhances the application's authentication flow

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->append(AuthMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
