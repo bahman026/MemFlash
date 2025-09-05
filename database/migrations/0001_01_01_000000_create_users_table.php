77478c67f4bbff8856a07b680d23a47ed22f83c7
bahman <bja@adlas.at>
2025-09-05 13:49:37 +0330

    (configfeat): add Google OAuth credentials and update session driver
    - Add Google OAuth credentials to services.php
    - Change session driver from database to file
    - Add avatar field to users table
    - Remove unused RolesEnum import in AdminSeeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
             $table->unsignedTinyInteger('status')->default(UserStatusEnum::ACTIVE->value);
             $table->rememberToken();
             $table->timestamps();
